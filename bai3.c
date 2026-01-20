cf23db958bb34ba75bbdb4b08c8d466a246fcfeb
havanthinhnb20-ai <havanthinhnb20@gmail.com>
2026-01-20 08:46:40 +0700

    noplap3

--- bai3.c
+++ bai3.c
@@ -1,17 +1,26 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Hà văn thinh]
+ * MSSV:      [ps49131]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TIỀN ĐIỆN 
 //  Input: Nhập vào số điện tiêu thụ hàng tháng 
 //  Output: Hiển thị số tiền cần phải đóng
- 
+
 #include <stdio.h>
 
 int main(){
-    
+    int soDien;
+    float tienDien;
+    int donGia = 3000;
+    printf("Nhập số tiền điện tiêu thụ hàng tháng: ");
+    scanf("%d", &soDien);
+    if(soDien <0)
+    printf("số điện không hợp lệ");
+    else
+    tienDien = soDien * donGia;
+    printf("Số tiền cần phải đóng là: %.3fvnd\n", tienDien);    
     // Khai báo biến
 
 
--- bai4.c
+++ bai4.c
@@ -10,7 +10,35 @@
 #include <stdio.h>
 
 int main(){
+    int choice;
+    printf("Menu Chương Trình:\n");
+    printf("1. Xây dựng chương trình tính học lực\n");
+    printf("2. Giải phương trình bậc 2\n");
+    printf("3. Tính tiền điện\n");
+    printf("0. Thoát\n");
+    printf("Chọn một tùy chọn (0-3): ");
+    scanf("%d", &choice);
+    switch(choice){
+        case 1:
+            printf("Bạn đã chọn chương tình tính học lực.\n");
+            break;
+        
+        case 2:
+            printf("Bạn đã chọn giải phương trình bậc 2.\n");
+            
+            break;
+        case 3:
+            printf("Bạn đã chọn tính tiền điện.\n");
+           
+            break;
+        case 0:
+            printf("Thoát chương trình. Tạm biệt!\n");
+            break;
+        default:
+            printf("Lựa chọn không hợp lệ. Vui lòng chọn lại.\n");
+    }
     
+
     // Khai báo biến
 
 
